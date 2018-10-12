16d577fd546f7cab72af7c02ac0cf5193a29014a
perillaroc <perillaroc@gmail.com>
2018-10-12 15:32:14 +0800

    :package: add setup.py for nwpc-workload-collector.
    :memo: update readme.
    :bug: fix project url for nwpc-ecflow-collector.

---
+++ setup.py
@@ -0,0 +1,57 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='nwpc-workload-collector',
+
+    version='0.1.0',
+
+    description='Collectors for workload systems at NWPC.',
+    long_description=long_description,
+
+    url='https://github.com/perillaroc/nwpc-system-collector',
+
+    author='perillaroc',
+    author_email='perillaroc@gmail.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6'
+    ],
+
+    keywords='nwpc collector loadleveler slurm',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    install_requires=[
+        'click',
+        'PyYAML',
+        'paramiko',
+        'requests',
+        'nwpc_hpc_model'
+    ],
+
+    package_data={
+        'nwpc_workload_collector': ['conf/*.yml']
+    },
+
+    extras_require={
+        'test': ['pytest'],
+    },
+
+    entry_points={
+        'console_scripts': [
+            'loadleveler_collector=nwpc_workload_collector.loadleveler.collector:cli',
+            'slurm_collector=nwpc_workload_collector.slurm.collector:cli',
+        ]}
+)
