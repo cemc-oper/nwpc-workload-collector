5c03531cf7258b528f8cfc8b609ca497f8278c79
perillaroc <perillaroc@gmail.com>
2018-11-27 21:27:30 +0800

    :sparkles: add gRPC server for sms collector. fix #10

---
+++ nwpc_workload_collector/slurm/server/slurm_collector_client.py
@@ -0,0 +1,90 @@
+# coding: utf-8
+import grpc
+import click
+
+from nwpc_workload_collector.slurm.server.proto import (
+    slurm_collector_pb2_grpc, slurm_collector_pb2)
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option('--rpc-target', '-t', help='rpc target')
+@click.option('--owner', help='owner name', required=True)
+@click.option('--repo', help='repo name', required=True)
+@click.option('--host', help='remote host')
+@click.option('--port', help='remote port', type=int, default=22)
+@click.option('--user', help='remote user')
+@click.option('--password', help='remote password')
+@click.option('--show', 'output_style', flag_value='show', default=True)
+@click.option('--post', 'output_style', flag_value='post')
+@click.option('--post-url', help='post url')
+@click.option('--gzip', 'content_encoding', flag_value='gzip', help='use gzip to post data.')
+@click.option('--verbose', is_flag=True, help='show more outputs', default=False)
+def jobs(rpc_target, owner, repo,
+         host, port, user, password,
+         output_style, post_url, content_encoding, verbose):
+
+    jobs_request = slurm_collector_pb2.JobsRequest(
+        owner=owner,
+        repo=repo,
+        host=host,
+        port=port,
+        user=user,
+        password=password,
+        output_style=output_style,
+        post_url=post_url,
+        content_encoding=content_encoding,
+        verbose=verbose
+    )
+
+    with grpc.insecure_channel(rpc_target) as channel:
+        stub = slurm_collector_pb2_grpc.SlurmCollectorStub(channel)
+        response = stub.CollectJobs(jobs_request)
+        print(response.status)
+
+    return
+
+
+@cli.command()
+@click.option('--rpc-target', '-t', help='rpc target')
+@click.option('--owner', help='owner name', required=True)
+@click.option('--repo', help='repo name', required=True)
+@click.option('--host', help='remote host')
+@click.option('--port', help='remote port', type=int, default=22)
+@click.option('--user', help='remote user')
+@click.option('--password', help='remote password')
+@click.option('--show', 'output_style', flag_value='show', default=True)
+@click.option('--post', 'output_style', flag_value='post')
+@click.option('--post-url', help='post url')
+@click.option('--gzip', 'content_encoding', flag_value='gzip', help='use gzip to post data.')
+@click.option('--verbose', is_flag=True, help='show more outputs', default=False)
+def queues(rpc_target, owner, repo,
+           host, port, user, password,
+           output_style, post_url, content_encoding, verbose):
+    queues_request = slurm_collector_pb2.QueuesRequest(
+        owner=owner,
+        repo=repo,
+        host=host,
+        port=port,
+        user=user,
+        password=password,
+        output_style=output_style,
+        post_url=post_url,
+        content_encoding=content_encoding,
+        verbose=verbose
+    )
+
+    with grpc.insecure_channel(rpc_target) as channel:
+        stub = slurm_collector_pb2_grpc.SlurmCollectorStub(channel)
+        response = stub.CollectQueues(queues_request)
+        print(response.status)
+
+    return
+
+
+if __name__ == "__main__":
+    cli()
---
+++ nwpc_workload_collector/slurm/server/slurm_collector_server.py
@@ -0,0 +1,123 @@
+# coding: utf-8
+from concurrent import futures
+import time
+import json
+import logging
+
+import click
+import grpc
+
+from nwpc_workload_collector.slurm.commands.jobs import collect_jobs
+from nwpc_workload_collector.slurm.commands.queues import collect_queues
+from nwpc_workload_collector.slurm.server.proto import (
+    slurm_collector_pb2, slurm_collector_pb2_grpc)
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+logging.basicConfig(
+    format='%(asctime)s %(message)s',
+    datefmt='[%Y-%m-%d %H:%M:%S]',
+    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+class SlurmCollectorService(slurm_collector_pb2_grpc.SlurmCollectorServicer):
+    def __init__(self, config):
+        self.config = config
+
+    def CollectJobs(self, request, context):
+        owner = request.owner
+        repo = request.repo
+        host = request.host
+        port = request.port
+        user = request.user
+        password = request.password
+        output_style = request.output_style
+        post_url = request.post_url
+        content_encoding = request.content_encoding
+        verbose = request.verbose
+
+        post_url = post_url.format(owner=owner, repo=repo)
+
+        logger.info('CollectJobs: {owner}/{repo}...'.format(owner=owner, repo=repo))
+
+        result = collect_jobs(
+            self.config,
+            owner, repo,
+            host, port, user, password,
+            output_style, post_url, content_encoding,
+            verbose
+        )
+        logger.info('CollectStatus: {owner}/{repo}...done'.format(owner=owner, repo=repo))
+
+        response = slurm_collector_pb2.Response(
+            status="ok"
+        )
+        if result is not None:
+            response.result = json.dumps(result)
+
+        return response
+
+    def CollectQueues(self, request, context):
+        owner = request.owner
+        repo = request.repo
+        host = request.host
+        port = str(request.port)
+        user = request.user
+        password = request.password
+        output_style = request.output_style
+        post_url = request.post_url
+        content_encoding = request.content_encoding
+        verbose = request.verbose
+
+        post_url = post_url.format(owner=owner, repo=repo)
+
+        logger.info('CollectJobs: {owner}/{repo}...'.format(owner=owner, repo=repo))
+
+        result = collect_queues(
+            self.config,
+            owner, repo,
+            host, port, user, password,
+            output_style, post_url, content_encoding,
+            verbose
+        )
+        logger.info('CollectQueues: {owner}/{repo}...done'.format(owner=owner, repo=repo))
+
+        response = slurm_collector_pb2.Response(
+            status="ok"
+        )
+        if result is not None:
+            response.result = json.dumps(result)
+
+        return response
+
+
+@click.command()
+@click.option('-t', '--rpc-target', help='rpc-target', default="[::]:50051")
+@click.option('-n', '--workers-num', help='max workers number', default=5, type=int)
+@click.option('--config', 'config_file_path',
+              help="collector's config file path", required=True)
+def serve(rpc_target, workers_num, config_file_path):
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=workers_num))
+
+    from nwpc_workload_collector.slurm.common.config import get_config
+    config = get_config(config_file_path)
+
+    slurm_collector_pb2_grpc.add_SlurmCollectorServicer_to_server(
+        SlurmCollectorService(config), server)
+    server.add_insecure_port('{rpc_target}'.format(rpc_target=rpc_target))
+    logger.info('listening on {rpc_target}'.format(rpc_target=rpc_target))
+    logger.info('starting slurm collector server...')
+    server.start()
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        logger.info('warm stop...')
+        server.stop(0)
+        logger.info('warm stop...done')
+
+
+if __name__ == "__main__":
+    serve()
